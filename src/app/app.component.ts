f55ca9e5227fc8b30b5bd9f0e4c7f19097abb1b0
Artur Ribeiro <artur.adr@hotmail.com>
2019-06-29 22:49:12 -0300

    Projeto finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'easyResourceFront';
+export class AppComponent implements OnInit {
+
+  columns: Array<any>;
+  page: number;
+  itemsPerPage: number;
+  maxSize: number;
+  numPages: number;
+  length: number;
+  config: any;
+  filterString: string;
+  data: Observable<any>;
+  rows: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.columns = [
+      { title: 'Sku', name: 'sku' },
+      { title: 'Name', name: 'name' },
+      { title: 'Details', name: 'details' },
+      { title: 'Informations', name: 'informations' },
+      { title: 'Tags', name: 'tags' },
+      { title: 'Price', name: 'price' },
+      { title: 'Sizes', name: 'sizes' },
+    ];
+    this.page = 1;
+    this.itemsPerPage = 10;
+    this.maxSize = 5;
+    this.numPages = 1;
+    this.length = 0;
+    this.config = {
+      paging: true,
+      sorting: { columns: this.columns },
+      filtering: { filterString: '' },
+      className: ['table-striped', 'table-bordered']
+    }
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+
+    let endpoint: string = environment.endpointAPI;
+    endpoint = this.filterString ? `${endpoint}?search=${this.filterString}` : endpoint;
+    endpoint = this.page ? `${endpoint}?page=${this.page}` : endpoint;
+
+    this.data = this.http.get(endpoint);
+    this.rows = this.data.pipe
+      (map(data =>
+        data.results.map(item => {
+          item.sizes = JSON.stringify(item.sizes);
+          item.informations = JSON.stringify(item.informations);
+          return item;
+        }))
+      );
+
+    this.data.pipe(map(data => data.count))
+      .subscribe(total => this.length = total);
+
+  }
+
+  public onFiltering(config) {
+    this.filterString = config.filtering.filterString;
+    this.getData();
+  }
+  
+  public pageChanged(event) {
+    if (this.page && event.page)
+      this.page = event.page;
+      console.log(this.page)
+      this.getData();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Ng2TableModule } from 'ng2-table/ng2-table';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    Ng2TableModule,
+    PaginationModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
